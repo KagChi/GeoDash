51a27a46311dfc251a1e0dda3c29511416bac36d
カグチ <59391215+KagChi@users.noreply.github.com>
2021-01-30 17:55:46 +0700

    Add files via upload

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,3 +1,12 @@
 extends "res://src/Actors/Actor.gd"
 
-func _physics_process
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
+
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
